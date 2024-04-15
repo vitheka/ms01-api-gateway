f1d132f89cabf413e1254ca9405f1f01305ce1eb
vitheka <vitorcoelhoft@gmail.com>
2024-04-15 18:41:21 -0300

    Create Api Gateway | RouteLocator

---
+++ src/main/java/br/com/vitor/ms01apigateway/Ms01ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitor.ms01apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms01ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms01ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vitor/ms01apigateway/Ms01ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitor.ms01apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms01ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
